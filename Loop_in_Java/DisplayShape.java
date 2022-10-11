17a08646ad610a0773e034f143086ac6c99bafb3
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-10-11 22:29:07 +0700

    Loop

---
+++ Loop_in_Java/DisplayShape.java
@@ -0,0 +1,54 @@
+package Loop_in_Java;
+
+import java.util.Scanner;
+
+public class DisplayShape {
+
+    public static void PrintRectangle(int height, int width) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println();
+
+    }
+
+    public static void PrintTriangleBotLeft(int height, int width) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println();
+
+    }
+
+    public static void PrintTriangleTopLeft(int height, int width) {
+        for (int i = 0; i < height; i++) {
+            for (int j = i; j < width; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input height = ");
+        int height = scanner.nextInt();
+        System.out.print("Input width = ");
+        int width = scanner.nextInt();
+        System.out.println();
+
+        PrintRectangle(height, width);
+        PrintTriangleBotLeft(height, width);
+        PrintTriangleTopLeft(height, width);
+    }
+
+}
