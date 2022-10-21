2a09528c17ea0d7a43f1aa58c87268b7372a963b
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-10-21 10:44:29 +0700

    update access modifier

---
+++ access_modifier/Circle.java
@@ -0,0 +1,35 @@
+package access_modifier;
+
+public class Circle {
+    private double radius = 1.0;
+    private final String COLOR = "red";
+
+    public Circle() {
+
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    private String getColor() {
+        return COLOR;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + COLOR + '\'' +
+                '}';
+    }
+
+}
---
+++ access_modifier/TestCircle.java
@@ -0,0 +1,34 @@
+package access_modifier;
+
+public class TestCircle {
+    public static void main(String[] args) {
+
+        Circle circleA = new Circle(4.0);
+        Circle circleB = new Circle();
+
+        System.out.print("Information of circleA: ");
+        System.out.println(circleA);
+        System.out.print("Radius of circleA: ");
+        System.out.println(circleA.getRadius());
+        System.out.print("Area of circleA: ");
+        System.out.println(circleA.getArea());
+        System.out.println();
+
+        System.out.print("Information of circleB: ");
+        System.out.println(circleB);
+        System.out.print("Radius of circleB: ");
+        System.out.println(circleB.getRadius());
+        System.out.print("Area of circleB: ");
+        System.out.println(circleB.getArea());
+        System.out.println();
+
+/**
+ *      Truy xuất phương thức getColor() bị lỗi do đã cài phạm vi truy cập là private
+ *      Đổi lại các pham vi truy cập khác như: public, protected, default sẽ truy cập được.
+ *
+ *         System.out.print("color of circleB: ");
+ *         System.out.println(circleB.getColor());
+ */
+
+    }
+}
