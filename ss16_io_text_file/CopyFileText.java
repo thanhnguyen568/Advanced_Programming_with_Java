77b9dcfc0b7689d294b4adf297fc6131c7a1379c
thanhnguyen568 <thanhnguyen230@gmail.com>
2022-11-25 23:08:29 +0700

    update ss16

--- ss16_io_text_file/CopyFileText.java
+++ ss16_io_text_file/CopyFileText.java
@@ -3,14 +3,17 @@ package ss16_io_text_file;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CopyFileText {
+    static Scanner scanner = new Scanner(System.in);
     private static final String FILE_PATH_SOURCE = "D:\\CodeGym\\Module2_Advanced_Programming_with_Java\\src\\ss16_io_text_file\\data\\sourcefile.csv";
     private static final String FILE_PATH_TARGET = "D:\\CodeGym\\Module2_Advanced_Programming_with_Java\\src\\ss16_io_text_file\\data\\targetfile.csv";
+    private static  List<Fruit> fruitArrayList = new ArrayList<>();
 
-    public static List<Fruit> readCSV() throws IOException {
+    public static List<Fruit> readCSV(String file_Patch) throws IOException {
         List<Fruit> fruitsList = new ArrayList<>();
-        FileReader fileReader = new FileReader(FILE_PATH_SOURCE);
+        FileReader fileReader = new FileReader(file_Patch);
         BufferedReader buffer = new BufferedReader(fileReader);
 
         String line;
@@ -37,15 +40,62 @@ public class CopyFileText {
         buffer.close();
     }
 
-    public static void main(String[] args) throws IOException {
-        List<Fruit> fruitList = readCSV();
+    public static Fruit information() {
+        System.out.println("Input no.: ");
+        int no = Integer.parseInt(scanner.nextLine());
+        System.out.println("Input name: ");
+        String name = scanner.nextLine();
+        System.out.println("Input price: ");
+        int price = Integer.parseInt(scanner.nextLine());
 
-        for (Fruit item: fruitList){
+        return new Fruit(no, name, price);
+    }
+
+    public static void createNew() throws IOException {
+        System.out.println("Create a new fruit...");
+        Fruit fruit = information();
+        // add vào List
+        fruitArrayList.add(fruit);
+        // Ghi vào file
+        writeCSV(fruit);
+        System.out.println("Create a new fruit successful.");
+    }
+
+    public static void updateObject() throws IOException {
+        int id;
+
+        do {
+            System.out.print("Input no. wanna edit=");
+            id = Integer.parseInt(scanner.nextLine());
+        } while (id < 1000);
+
+        boolean found = false;
+        for (int i = 0; i < fruitArrayList.size(); i++) {
+            if (id == fruitArrayList.get(i).getNo()) {
+                fruitArrayList.set(i, information());
+                found = true;
+                System.out.println("Edit successful!");
+            }
+        }
+        if (!found) {
+            System.out.println("Not found");
+        }
+        //..
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        fruitArrayList = readCSV(FILE_PATH_SOURCE);
+
+        for (Fruit item : fruitArrayList) {
             // Đọc file
             System.out.println(item);
-            // Viết file
+            // Ghi file
             writeCSV(item);
         }
+
+//        createNew();
+        updateObject();
     }
 
 }
